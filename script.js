3186148a2da21bd666d028e5ae6b27a86329de5b
soakb432 <107282874+SoakB@users.noreply.github.com>
2024-08-20 19:23:39 -0400

    Restructure pseudocode

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-//FUNCTION getComputerChoice {
-//  CHOOSE random integer: 0, 1 or 2
-//
-//  FUNCTION getRandomInteger(max) {
-//    [Math.random()] RETURNS number between 0 and less than 1
-//    ["max" is the maximum integer the computer can choose (2)]
-//    [Math.floor()] TAKES a number and rounds it down to the nearest integer
+//FUNCTION getRandomInteger(max) {
+//  [Math.random()] RETURNS number between 0 and less than 1
+//  ["max" is the largest integer the computer can choose PLUS 1]
+//  [Math.floor()] TAKES a number and rounds it down to the nearest integer
 //    
-//    GET random number between 0 and 1
-//    MULTIPLY random number with "max"
-//    ROUND DOWN the result
-//    RETURN an integer between 0 and "max"
-//  }
-//
-//  SAVE random integer in a variable "choice"
+//  GET random number between 0 and 1
+//  MULTIPLY random number with "max"
+//  ROUND DOWN the result
+//  RETURN an integer between 0 and "max" MINUS 1
+// }
+
+//FUNCTION getComputerChoice {
+//  CREATE variable "choice"
+//  CHOOSE random integer: 0, 1 or 2 AND STORE it in "choice"
 //  IF choice EQUALS 0 RETURN “rock”
 //  ELSE IF choice EQUALS 1 RETURN "paper"
 //  ELSE IF choice EQUALS 2 RETURN “scissors”
-//  }
+// }
